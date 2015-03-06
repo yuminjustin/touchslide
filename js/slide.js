392c7ed8a93ffdc0e2aad09f75df1d08d58f4ebe
justin lee <yuminjustin@163.com>
2015-03-06 15:07:21 +0800

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,152 +1,144 @@
 (function ($) {
     var s, s2, c, n, autoplay = true,
         timeout = 3000,
+        fingerfollow = true,
         touchslide = function (opts) {
             s = opts.s, s2 = opts.s2, c = opts.c, n = $("img", s2).length;
             if (typeof (opts.a) != "undefined") autoplay = opts.a;
             if (typeof (opts.t) == "number" && opts.t > 1000) timeout = opts.t;
+            if (typeof (opts.f) != "undefined") fingerfollow = opts.f;
             init();
-            events();
         },
+        oldtimeout = timeout,
         w = $(window).width(),
         h = $(window).height(),
-        li, liOn, l1, l2, l3, po, g = 0,
+        g = 0,
+        li = $("li", s2),
+        l, po,
         init = function () {
-            s2.height(h);
-            if (n == 1) return false;
-            if (n <= 2 && n > 1) {
-                s2.append(s2.html());
-            }
-            s.width(w).height(h);
-            li = $("li", s2);
-            li.width(w).height(h);
-            commonOpts.rollback();
-            for (var i = 0; i < n; i++) c.append("<span></span>");
-            $("span:eq(0)", c).addClass("sOn");
-            c.css("margin-left", c.width() * -0.5);
-            for (var i = 0, l = li.length; i < l; i++) {
-                var temp = li[i];
-                if (i < (l - 1))
-                    temp.sn = i + 1;
-                else
-                    temp.sn = 0;
-                if (i > 0)
-                    temp.sp = i - 1;
-                else
-                    temp.sp = l - 1;
-                temp.style.display = "none";
-            }
-            liOn = li[0];
-            $(liOn).show().css("left", 0);
-            $(li[liOn.sn]).show().css("left", w);
-            $(li[liOn.sp]).show().css("left", -w);
-            if (autoplay)
-                autoplay = setTimeout(function () {
-                    slideplay()
-                }, timeout);
+            styles.stage();
+            events();
+            if (autoplay) auto.on();
         },
-        slideplay = function () {
-            animate.move({
-                p: "-=" + w,
-                n: "-=" + w,
-                o: "-=" + w
-            }, animate.next);
-            if (autoplay)
-                autoplay = setTimeout(function () {
-                    slideplay()
+        auto = {
+            on: function () {
+                autoplay = setInterval(function () {
+                    animate.next();
                 }, timeout);
+            },
+            off: function () {
+                clearInterval(autoplay);
+            }
         },
-        dote = function () {
-            var o = $(".sOn", c);
-            var m = $("span:eq(" + g + ")", c);
-            m.addClass("sOn");
-            o.removeClass("sOn");
+        styles = {
+            stage: function () {
+                s.width(w).height(h);
+                this.dot();
+                this.play(s2, -w);
+                this.transit(0.3);
+                s2.append($(li[0])[0].outerHTML);
+                $(li[0]).before($(li[n - 1])[0].outerHTML);
+                for (var i = 0; i < (n + 2); i++) this.play($("li:eq(" + i + ")", s2), i * w);
+            },
+            transit: function (t) {
+                s2.css({
+                    "-webkit-transition": "all " + t + "s",
+                    "-moz-transition": "all " + t + "s",
+                    "transition": "all " + t + "s"
+                });
+            },
+            play: function (o, i) {
+                o.css({
+                    "transform": "translate(" + i + "px,0px)",
+                    "-moz-transform": "translate(" + i + "px,0px)",
+                    "-webkit-transform": "translate(" + i + "px,0px)"
+                });
+            },
+            dot: function () {
+                for (var i = 0; i < n; i++) c.append("<span></span>");
+                $("span:eq(0)", c).addClass("sOn");
+                c.css("margin-left", c.width() * -0.5);
+            },
+            dotchange: function () {
+                var x = g;
+                if (g == n) x = 0;
+                $(".sOn", c).removeClass("sOn");
+                $("span:eq(" + x + ")", c).addClass("sOn");
+            }
         },
         animate = {
             next: function () {
-                $(li[liOn.sp]).hide();
-                liOn = li[liOn.sn];
-                $(li[liOn.sn]).show().css("left", w);
-                if (g < (n - 1)) g++;
-                else g = 0;
-                dote();
-                commonOpts.rollback();
+                g++;
+                if (g == (n + 1)) {
+                    g = 0;
+                    styles.transit(0);
+                    styles.play(s2, -w);
+                    setTimeout(this.next, 100);
+                    return;
+                } else {
+                    styles.transit(0.3);
+                }
+                styles.play(s2, (g + 1) * -w);
+                styles.dotchange();
             },
             prev: function () {
-                $(li[liOn.sn]).hide();
-                liOn = li[liOn.sp];
-                $(li[liOn.sp]).show().css("left", -w);
-                if (g > 0) g--;
-                else g = n - 1;
-                dote();
-                commonOpts.rollback();
-            },
-            move: function (data, fn) {
-                commonOpts.transition();
-                $(li[liOn.sp]).css({
-                    "left": data.p
-                });
-                $(li[liOn.sn]).css({
-                    "left": data.n
-                });
-                $(liOn).css({
-                    "left": data.o
-                });
-                transitEnd(liOn, fn);
+                g--;
+                if (g == -1) {
+                    g = n - 1;
+                    styles.play(s2, 0);
+                    styles.dotchange();
+                    transitEnd(s2[0], function () {
+                        styles.transit(0);
+                        styles.play(s2, (g + 1) * -w);
+                    });
+                    return;
+                } else {
+                    styles.transit(0.3);
+                }
+                styles.play(s2, (g + 1) * -w);
+                styles.dotchange();
             }
         },
         events = function () {
-            s2.on("touchstart", function () {
-                clearTimeout(autoplay);
-                l1 = parseInt($(li[liOn.sp]).css("left"));
-                l2 = parseInt($(liOn).css("left"));
-                l3 = parseInt($(li[liOn.sn]).css("left"));
+            s.on("touchstart", function (e) {
+                if (autoplay) auto.off();
+                l = (g + 1) * -w;
             });
-            s2.drag(function (event, p) {
-                $(li[liOn.sp]).css({
-                    "left": l1 + (p.x - p.xs)
+            if (!fingerfollow) {
+                s.swipeLeft(function () {
+                    animate.next();
                 });
-                $(liOn).css({
-                    "left": l2 + (p.x - p.xs)
+                s.swipeRight(function () {
+                    animate.prev();
                 });
-                $(li[liOn.sn]).css({
-                    "left": l3 + (p.x - p.xs)
-                });
-                if (Math.abs(p.x - p.xs) >= w * 0.5) {
-                    if ((p.x - p.xs) < 0) {
-                        po = "next";
+            } else {
+                s.drag(function (event, p) {
+                    styles.transit(0);
+                    styles.play(s2, l + (p.x - p.xs));
+                    if (Math.abs(p.x - p.xs) >= w * 0.38) {
+                        if ((p.x - p.xs) < 0) {
+                            po = "next";
+                        } else {
+                            po = "prev";
+                        }
                     } else {
-                        po = "prev";
+                        po = "none";
                     }
-                } else {
-                    po = "none";
+                });
+            }
+            s.on("touchend", function () {
+                if (fingerfollow) {
+                    styles.transit(0.3);
+                    if (po == "prev") {
+                        animate.prev();
+                    } else if (po == "next") {
+                        animate.next();
+                    } else {
+                        styles.play(s2, l);
+                    }
                 }
-            })
-            s2.on("touchend", function (event) {
-                event.preventDefault();
-                if (po == "prev") {
-                    animate.move({
-                        p: 0,
-                        n: 2 * w,
-                        o: w
-                    }, animate.prev);
-                } else if (po == "next") {
-                    animate.move({
-                        p: -2 * w,
-                        n: 0,
-                        o: -w
-                    }, animate.next);
-                } else {
-                    animate.move({
-                        p: l1,
-                        n: l3,
-                        o: l2
-                    }, function () {});
-                }
-                if (autoplay)
-                    autoplay = setTimeout(function () {
-                        slideplay()
-                    }, timeout);
+                if (autoplay) auto.on();
+
             });
         },
         transitEnd = function (obj, fn) {
@@ -157,22 +149,6 @@
             }
             obj.addEventListener("webkitTransitionEnd", transitoff, false);
             obj.addEventListener("transitionend", transitoff, false);
-        },
-        commonOpts = {
-            rollback: function () {
-                li.css({
-                    "-webkit-transition": "all 0s",
-                    "-moz-transition": "all 0s",
-                    "transition": "all 0s"
-                });
-            },
-            transition: function () {
-                li.css({
-                    "-webkit-transition": "all 0.8s",
-                    "-moz-transition": "all 0.8s",
-                    "transition": "all 0.8s"
-                });
-            }
-        }
+        };
     window.touchslide = touchslide;
 })(jQuery);
